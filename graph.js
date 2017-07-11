6deb27f2d244fbf7559942136480b3dff1191dff
fiatjaf <fiatjaf@gmail.com>
2017-07-11 15:15:46 +0000

    adjustable ratio and output format. URLs for modules.

--- graph.js
+++ graph.js
@@ -1,4 +1,3 @@
-const Viz = require('viz.js')
 const download = require('co-download')
 const decompress = require('decompress')
 const mkdirp = require('mkdirp')
@@ -6,12 +5,9 @@ const rmr = require('rmr')
 
 const dep = require('./dependency-tree')
 
-var cache = {}
-var base
-
 module.exports = function (user, repo) {
   let tmpfolder = '/tmp/' + user + '/' + repo
-  base = tmpfolder + '/' + repo + '-master'
+  let base = tmpfolder + '/' + repo + '-master'
   mkdirp.sync(tmpfolder)
 
   return download('https://github.com/' + user + '/' + repo + '/archive/master.zip', '/tmp/master.zip')
@@ -20,22 +16,20 @@ module.exports = function (user, repo) {
     let rels = dep(base)
 
     var dot = 'digraph main {\n'
-    dot += '  size="400,800"\n'
-    dot += '  ratio="compress"\n'
     for (let i = 0; i < rels.length; i++) {
       let [from, kind, to] = rels[i]
       dot += `  "${from}"->"${to}"` + '\n'
 
-      if (kind === 'external' && !(to in cache)) {
-        dot += `  "${to}" [shape="box"]` + '\n'
+      dot += `  "${from}" [URL="https://github.com/${user}/${repo}/blob/master/${from.slice(2)}"]` + '\n'
+      if (kind === 'external') {
+        dot += `  "${to}" [shape="box", URL="https://npmjs.com/package/${to}"]` + '\n'
+      } else if (kind === 'local') {
+        dot += `  "${to}" [URL="https://github.com/${user}/${repo}/blob/master/${to.slice(2)}"]` + '\n'
       }
     }
     dot += '}'
-    console.log(dot)
-
     rmr.sync(tmpfolder)
-
-    return Viz(dot)
+    return dot
   })
   .catch(console.log)
 }
--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 const express = require('express')
+const md5 = require('md5')
+const Viz = require('viz.js')
 
 const graph = require('./graph')
 
 const app = express()
 
 app.get('/:user/:repo', (r, w) => {
-  graph(r.params.user, r.params.repo)
-    .then(svg => w.send(svg))
+  var format = 'svg'
+  var repo = r.params.repo
+  let rsplitted = r.params.repo.split('.')
+  if (rsplitted.length > 1) {
+    let f = rsplitted[rsplitted.length - 1]
+    if (f === 'dot' || f === 'svg') {
+      format = f
+      repo = rsplitted.slice(0, -1).join('.')
+    }
+  }
+
+  graph(r.params.user, repo)
+    .then(dot => {
+      if (format === 'svg') {
+        let ratio = r.query.ratio || '0.5'
+        let lines = dot.split('\n')
+        dot = [lines[0], `ratio="${ratio}"`].concat(lines.slice(1)).join('\n')
+
+        w.append('Cache-Control', 'no-cache')
+        w.append('ETag', md5(dot))
+        w.send(Viz(dot))
+      } else if (format === 'dot') {
+        w.send(dot)
+      } else {
+        w.send('this will never happen.')
+      }
+    })
     .catch(console.log)
 })
 
